fc0734538c67efc2d850ed33e57ecb6137449bd8
GritworkTMH <gritworktmh@gmail.com>
2024-01-24 14:55:18 +0000

    updated version

--- versions.tf
+++ versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.12.0"
+      version = "5.33.0"
     }
   }
 }
 
 provider "aws" {
-  shared_config_files      = ["/home/vagrant/.aws/config"]
-  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
-  profile                  = "bca-aws-master"
-  #   alias                    = "gritworks-nonprod"
+  # shared_config_files      = ["/home/vagrant/.aws/config"]
+  # shared_credentials_files = ["/home/vagrant/.aws/credentials"]
+  # profile                  = "iamadmin-master"
+  # #   alias                = "gritworks-nonprod"
   region = var.aws_region
 }
